8606e716cb083d20364c09c26a6f4024fbf79991
abdieljrg <abdieljrg@gmail.com>
2024-05-09 15:58:07 -0400

    gg 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
-    mayusculas = "QWERTYUIOPASDFGHJKLZXCVBNM"
-    minusculas = "qwertyuiopasdfghjklzxcvbnm"
+    mayusculas = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+    minusculas = 'qwertyuiopasdfghjklzxcvbnm'
     if c in minusculas:
         return True
     else:
         return False
-    
