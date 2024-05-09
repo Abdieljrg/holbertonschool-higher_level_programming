a4479e51ec299e557c336352c980ca173d2773f7
abdieljrg <abdieljrg@gmail.com>
2024-05-09 15:56:11 -0400

    7 new

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def islower(c):
+    mayusculas = "QWERTYUIOPASDFGHJKLZXCVBNM"
+    minusculas = "qwertyuiopasdfghjklzxcvbnm"
+    if c in minusculas:
+        return True
+    else:
+        return False
+    
