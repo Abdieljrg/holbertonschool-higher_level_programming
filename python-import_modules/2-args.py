2d8b09cc8e82ef11f294213d86197e777079195d
abdieljrg <abdieljrg@gmail.com>
2024-05-10 20:53:17 -0400

    PLEASEEEEEEE

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
-    if count == 1:
+    elif count == 1:
         print("1 argument:")
         print(f"1: {sys.argv[1]}")
     else:
