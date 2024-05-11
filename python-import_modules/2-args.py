e0dacd9900e7af31289f3e97d3d3d35fb066a3b9
abdieljrg <abdieljrg@gmail.com>
2024-05-10 20:49:40 -0400

    PLEAAAAAAAAAASE

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     else:
         print(f"{count} arguments:")
         for i in range(1, count + 1):
-            print(f"{1}: {sys.argv[1]}")
+            print(f"{i}: {sys.argv[i]}")
