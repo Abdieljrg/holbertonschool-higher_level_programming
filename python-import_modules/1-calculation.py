ee262aa8efecac6f694903ce6c84449bd9fe073e
abdieljrg <abdieljrg@gmail.com>
2024-05-10 20:19:57 -0400

    1 ez 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,4 +4,13 @@ if __name__ == "__main__":
     a = 10
     b = 5
     result = add(a, b)
-print("{} {} {}".format(a, b, result))
+print("{} + {} = {}".format(a, b, result))
+
+result = sub(a, b)
+print("{} - {} = {}".format(a, b, result))
+
+result = mul(a, b)
+print("{} * {} = {}".format(a, b, result))
+
+result = div(a, b)
+print("{} / {} = {}".format(a, b, result))
