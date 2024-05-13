3e086896dfb7c28d6fb3ed8f6dd894770685e911
abdieljrg <abdieljrg@gmail.com>
2024-05-13 15:05:12 -0400

    6 new

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col_idx, col in enumerate(row):
+            if col_idx != 0:
+                print(" ", end="")
+            print("{:d}".format(col), end="")
+        print("$")
+    if not matrix:
+        print("$")
