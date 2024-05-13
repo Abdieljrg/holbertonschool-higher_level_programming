0a726cc7056a80901314934805a88ab58bcd54a9
abdieljrg <abdieljrg@gmail.com>
2024-05-13 12:17:21 -0400

    please be right

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+def element_at(my_list, idx):
+    if idx < 0:
+        return (None)
+    if idx >= len(my_list, idx):
+        return (None)
+    else:
+        print("{:d}".format(my_list, idx))
+        return (my_list, idx)
