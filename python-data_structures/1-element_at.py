d652da0bc42241940e303d4e6c6a33a64e4a1a05
abdieljrg <abdieljrg@gmail.com>
2024-05-13 12:28:17 -0400

    please be good 3

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return (None)
-    if idx >= len(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
         return (None)
     else:
-        print("{:d}".format(my_list, idx))
-        return (my_list, idx)
+        print("{:d}".format(my_list[idx]))
+        return (my_list[idx])
