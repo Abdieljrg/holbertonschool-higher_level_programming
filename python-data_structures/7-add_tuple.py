98868bbcd5e6f06628c017d0a4df8d48b87cee24
abdieljrg <abdieljrg@gmail.com>
2024-05-14 09:04:09 -0400

    7 fix pycode

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,6 +4,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     b = tuple_b[:2] + (0, 0)
     sum_tuple = (a[0] + b[0], a[1] + b[1])
     return sum_tuple
+
+
 if __name__ == "__main__":
     tuple_a = (1, 89)
     tuple_b = (88, 11)
