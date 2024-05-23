8cae1335935241fd3f5f42c0211b11438192079e
Abdieljrg <abdieljrg@gmail.com>
2024-05-22 21:37:28 -0400

    1 new new

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" /"MyList" class inherits from list class """
+
+
+class MyList(list):
+    """ MyList inherits from list. """
+    def print_sorted(self):
+        """ Prints the list: ascending order. """
+        sorted_list = sorted(self)
+        print(sorted_list)
