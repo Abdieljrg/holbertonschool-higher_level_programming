b7b72d53f9eb8f680ca49aa8b1aa43ff0e941e1b
Abdieljrg <abdieljrg@gmail.com>
2024-05-28 23:55:21 -0400

    fixing agaaain

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,6 +9,8 @@ if __name__ == "__main__":
     save_to_json = __import__('5-save_to_json_file').save_to_json_file
     load_from_json = __import__('6-load_from_json_file').load_from_json_file
 
+    """loads and save"""
+
 try:
     data = load_from_json("add_item.json")
 except FileNotFoundError:
