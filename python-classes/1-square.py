2dc5c113d85ced178c87fcfd4423790a7b1a38ca
Abdieljrg <abdieljrg@gmail.com>
2024-05-31 15:33:08 -0400

    1 new

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" empty square"""
+
+
+class Square:
+    """ empty class Square"""
+
+    def __init__(self, size):
+        """ initialize a square class eith attribute"""
+        self.__size = size
